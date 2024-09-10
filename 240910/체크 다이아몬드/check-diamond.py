87f6f8d6b8aa412dd7fc19a9e4fb44962d858c0c
이규민 <90364652+20210815@users.noreply.github.com>
2024-09-10 15:06:55 +0900

    Title: 체크 다이아몬드, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240910/체크 다이아몬드/check-diamond.py
@@ -0,0 +1,24 @@
+n = int(input())
+
+# 공백 3 출력 1
+# 공백 2 출력 2
+# 공백 1 출력 3
+# 공백 0 출력 4
+
+# 공백 1 출력 3
+# 공백 2 출력 2
+# 공백 3 출력 1
+
+for i in range(1, n+1):
+    for k in range(n - i):
+        print(" ", end="")
+    for j in range(i):
+        print("*", end=" ")
+    print()
+
+for i in range(1, n+1):
+    for k in range(i):
+        print(" ", end="")
+    for j in range(n-i):
+        print("*", end=" ")
+    print()
