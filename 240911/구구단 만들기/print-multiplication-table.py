62aaca542a27dd3d9c608444dc6654aad2cabaa5
이규민 <90364652+20210815@users.noreply.github.com>
2024-09-11 15:40:08 +0900

    Title: 구구단 만들기, Time: 102ms, Memory: 24MB, Status: Passed - CodeTree

--- 240911/구구단 만들기/print-multiplication-table.py
+++ 240911/구구단 만들기/print-multiplication-table.py
@@ -5,8 +5,8 @@ a = int(sp[0])
 b = int(sp[1])
 
 for i in range(1, 10):
-    for j in range(b, 1, -a):
+    for j in range(b, a-1, -2):
         print(f"{j} * {i} = {i*j}", end="")
-        if j > 2:
+        if j > a:
             print("", end=" / ")
     print()
