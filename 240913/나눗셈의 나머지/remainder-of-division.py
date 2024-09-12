369033d8e22dec1df6fedcd75a59c3164f70a76b
이규민 <90364652+20210815@users.noreply.github.com>
2024-09-13 00:24:44 +0900

    Title: 나눗셈의 나머지, Time: 137ms, Memory: 24MB, Status: Passed - CodeTree

--- 240913/나눗셈의 나머지/remainder-of-division.py
+++ 240913/나눗셈의 나머지/remainder-of-division.py
@@ -1,15 +1,13 @@
 arr = list(map(int,input().split()))
 
 modu = [0] * 10
-i = 0
 while True:
     if arr[0] <= 1:
         break
     else:
         result = arr[0] % arr[1]
-        modu[result] = result
+        modu[result] += 1
         arr[0] = arr[0] // arr[1]
-        i += 1
 sum = 0
 for i in range(len(modu)):
     sum += modu[i] **2
