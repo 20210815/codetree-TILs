e5b516cc8eb41fbed1c908bd062e5f12ea7f2a5a
이규민 <90364652+20210815@users.noreply.github.com>
2024-09-13 15:24:07 +0900

    Title: 배열 놀이, Time: 139ms, Memory: 25MB, Status: Passed - CodeTree

--- 240913/배열 놀이/play-with-array.py
+++ 240913/배열 놀이/play-with-array.py
@@ -8,13 +8,7 @@ for i in range(nq[1]):
         print(arr[(que[1]-1)])
     elif que[0] == 2:
         if que[1] in arr:
-            if arr.count(que[1]) > 1:
-                for j in range(len(arr)):
-                    if arr[j] == que[1]:
-                        break
-                print(j+1) #몇번째 원소인지
-            else:
-                print(arr.index(que[1])+1) #몇번째 원소인지
+            print(arr.index(que[1])+1) #몇번째 원소인지
         else:
             print(0)
     else:
