5c914b9a1a28a4483fdb5b56b262555ff673d794
이규민 <90364652+20210815@users.noreply.github.com>
2024-09-13 21:44:03 +0900

    Title: 연속부분수열일까, Time: 137ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240913/연속부분수열일까/contiguous-array-or-not.py
+++ 240913/연속부분수열일까/contiguous-array-or-not.py
@@ -2,15 +2,32 @@ arr_ind = list(map(int, input().split()))
 
 a = list(map(int, input().split()))
 b = list(map(int, input().split()))
+# 1 5 2 6
+# 5 6
 
+
+# 4 1 3 1 3 1 4
+
+
+# 9 2 4 1 3 3 1 4 6 8
+# 4 1 3 1 3 1 4
+
+cnt = 1
 if b[0] in a:
-    ind = a.index(b[0])
-    for i in range(ind, arr_ind[0]): # 4 10 4 5 6 /  0 2
-        if a[i] != b[i-ind]:
-            print("No")
-            break
-        elif i-ind == len(b)-1:
-            print("Yes")
-            break
-else:
+    for i in range(a.index(b[0])+1, len(a)):
+        if a[i] == b[cnt]:
+            if len(b)-1 ==  cnt:
+                print("Yes")
+                break
+            else:
+                cnt += 1
+                continue
+        else:
+            if b[0] in a[i:]:
+                a[i:].index(b[0])
+            else:
+                print("No")
+                break
+    
+else: 
     print("No")
