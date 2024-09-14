02f253c1a37abfd147d0cf160ae1cd4518d0976f
이규민 <90364652+20210815@users.noreply.github.com>
2024-09-14 22:55:14 +0900

    Title: 격자 반대로 채우기, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240914/격자 반대로 채우기/grid-anti-fill.py
@@ -0,0 +1,26 @@
+n = int(input())
+
+# (3,3) (2,3) (1,3) (0,3)
+# (0,2) (1,2) (2,2) (3,2)
+# 3 2 1 0 
+
+# 1 7 차이
+
+arr = [[0 for _ in range(n)] for _ in range(n)]
+
+
+cnt = 1
+for i in range(n-1, -1, -1):
+    if i % 2 == 1:
+        for j in range(n-1, -1, -1):
+            arr[j][i] = cnt
+            cnt += 1
+    else:
+        for j in range(n):
+            arr[j][i] = cnt
+            cnt += 1
+
+for i in range(n):
+    for j in range(n):
+        print(arr[i][j], end= " ")
+    print()
