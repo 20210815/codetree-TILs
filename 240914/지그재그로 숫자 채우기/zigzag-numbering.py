92b5a5da0b40d5fad3d170efb1671b7eea2c0f97
이규민 <90364652+20210815@users.noreply.github.com>
2024-09-14 21:51:32 +0900

    Title: 지그재그로 숫자 채우기, Time: 133ms, Memory: 25MB, Status: Passed - CodeTree

--- 240914/지그재그로 숫자 채우기/zigzag-numbering.py
+++ 240914/지그재그로 숫자 채우기/zigzag-numbering.py
@@ -1,17 +1,19 @@
 n = list(map(int, input().split()))
+# n[0] 행, n[1] 열
 
 arr = [[0 for _ in range(n[1])] for _ in range(n[0])]
 
-k = 0
-for i in range(n[1]): #열
-    if i % 2 == 0:
-        for j in range(n[0]):
+# 0 (4*i(4-0)-1)7 8 15
+# 1 6 9 14
+# 2 5 10 13
+# 3 4 11 12
+for i in range(n[1]): #6
+    for j in range(n[0]): #4
+        if i % 2 == 0:
             arr[j][i] = i * n[0] + j
-    else:
-        for j in range(n[0]-1, -1, -1):
-            arr[k][i] = i * n[0] + j
-            k += 1
-            
+        else:
+            arr[j][i] = n[0] * i + (n[0] - j - 1)
+
 for i in range(n[0]):
     for j in range(n[1]):
         print(arr[i][j], end=" ")
