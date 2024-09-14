097158f8b6d2d8e74c74bbe2e489f0ed6517d0bc
이규민 <90364652+20210815@users.noreply.github.com>
2024-09-15 00:56:27 +0900

    Title: 문자에 따른 명령 2, Time: 120ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240915/문자에 따른 명령 2/text-based-commands2.py
+++ 240915/문자에 따른 명령 2/text-based-commands2.py
@@ -1,20 +1,24 @@
 n = input()
 # 3 x 1
 
-# 2 
-# y
-# 4
+#   2 
+# 3 y 1
+#   4
 
 r = 2
 x = 0
 y = 0
 for i in range(len(n)): #몇 번인지
     if n[i] == 'L':
-        r += 1
-        r %= 4
+        if r == 4:
+            r = 1
+        else :
+            r += 1
     elif n[i] == 'R':
-        r -= 1
-        r %= 4
+        if r == 1:
+            r = 4
+        else:
+            r -= 1
     elif n[i] == 'F':
         if r == 1:
             y += 1
