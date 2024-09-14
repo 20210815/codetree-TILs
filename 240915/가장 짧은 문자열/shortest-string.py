49d55e0c9edc0e89dc6e2532196c7a69b70bf59b
이규민 <90364652+20210815@users.noreply.github.com>
2024-09-15 03:13:16 +0900

    Title: 가장 짧은 문자열, Time: 134ms, Memory: 24MB, Status: Passed - CodeTree

--- 240915/가장 짧은 문자열/shortest-string.py
+++ 240915/가장 짧은 문자열/shortest-string.py
@@ -2,16 +2,8 @@ n = len(input())
 m = len(input())
 o = len(input())
 
-max = n
-min = n
+max(n,m,o)
 
-if max < m:
-    max = m
-    if min > o:
-        min = o
-if max < o:
-    max = o
-    if min > m:
-        min = m
+print(max(n,m,o) - min(n,m,o))
 
-print(max - min)
+#print(max - min)
