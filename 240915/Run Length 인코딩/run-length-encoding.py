f3b78bdceb834ddd167db491dc2caeaa90bfafd2
이규민 <90364652+20210815@users.noreply.github.com>
2024-09-15 15:13:25 +0900

    Title: Run Length 인코딩, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240915/Run Length 인코딩/run-length-encoding.py
+++ 240915/Run Length 인코딩/run-length-encoding.py
@@ -3,18 +3,22 @@ li = []
 cnt = 1
 sum = 0
 ch = string[0]
-for i in range(len(string)-1): #aaabbbbcbb
-    if ch == string[i+1]:
-        cnt += 1
-        if i == len(string)-2:
-            li.append([ch, cnt])
+if len(string) == 1:
+    print(2)
+    print(f"{ch}{cnt}", end="")
+else:
+    for i in range(len(string)-1): #aaabbbbcbb
+        if ch == string[i+1]:
+            cnt += 1
+            if i == len(string)-2:
+                li.append([ch, cnt])
         
-    else:
-        li.append([ch,cnt])
-        ch = string[i+1]
-        cnt = 1
-        if i == len(string)-2:
-            li.append([ch, cnt])
+        else:
+            li.append([ch,cnt])
+            ch = string[i+1]
+            cnt = 1
+            if i == len(string)-2:
+                li.append([ch, cnt])
 
 print(len(li)*2)
 for i in range(len(li)):
