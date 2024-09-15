bf1b48488ed1629921c875fd2413f2baeb71d382
이규민 <90364652+20210815@users.noreply.github.com>
2024-09-15 19:26:55 +0900

    Title: 두 번의 연산, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 240915/두 번의 연산/two-operations.py
+++ 240915/두 번의 연산/two-operations.py
@@ -2,12 +2,9 @@ n = int(input())
 
 if n % 2 == 1:
     n += 3
-    if n % 3 == 0:
-        print("%d" %(n//3))
-    else:
-        print(n)
-else:
-    if n % 3 == 0:
-        print("%d" %(n//3))
-    else:
-        print(n)
+
+
+if n % 3 == 0:
+    n //= 3
+
+print(n)
