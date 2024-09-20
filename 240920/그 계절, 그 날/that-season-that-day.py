bbdae44864e65eee8b451db38a4377e3f1c97f09
이규민 <90364652+20210815@users.noreply.github.com>
2024-09-20 19:22:04 +0900

    Title: 그 계절, 그 날, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240920/그 계절, 그 날/that-season-that-day.py
@@ -0,0 +1,42 @@
+def date_exists(y, m, d):
+    if m > 12 or d > 31:
+        return -1
+    else:
+        if m == 2:
+            if four_years(y):
+                if d > 29:
+                    return -1
+        elif m == 8:
+            if d > 31:
+                return -1
+        elif m % 2 == 0:
+            if d > 30:
+                return -1
+        else:
+            if d > 31:
+                return -1
+        return weather(m)
+
+def weather(m):
+    if m >= 3 and m <= 5:
+        return "Spring"
+    elif m >= 6 and m <= 8:
+        return "Summer"
+    elif m >= 9 and m <= 11:
+        return "Fall"
+    else:
+        return "Winter"
+    
+
+def four_years(y):
+    if y % 4 == 0:
+        if y % 100 == 0 and y % 400 == 0:
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
+y, m ,d = list(map(int, input().split()))
+print(date_exists(y, m, d))
