0784de38f44bad47b0d22f26d7ae21ce3649478e
이규민 <90364652+20210815@users.noreply.github.com>
2024-09-24 16:59:00 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240924/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
+++ 240924/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
@@ -9,3 +9,18 @@ def get_lmd(n):
     if n == 0:
         return arr[0]
     return arr[n] * get_lmd(n-1)
+
+
+def get_gcd(n):
+    gcd = 1
+    for i in range(n): #n번 돌면서...
+        for k in range(1,arr[i]+1):
+            if arr[i] % k != 0:
+                break
+        gcd = i
+        arr[i] = gcd
+
+get_gcd(n)
+print(get_lmd(n))
+
+#1 2 3 5 7 3
