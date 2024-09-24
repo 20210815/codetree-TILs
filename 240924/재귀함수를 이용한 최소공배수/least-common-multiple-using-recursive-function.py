717b8e32925114f7419a3ac796c9e34d02f7df4e
이규민 <90364652+20210815@users.noreply.github.com>
2024-09-24 16:53:40 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240924/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
+++ 240924/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
@@ -6,6 +6,6 @@ arr = list(map(int, input().split()))
 #       2   3   4    5
 
 def get_lmd(n):
-    if n == 1:
-        return arr[1]
+    if n == 0:
+        return arr[0]
     return arr[n] * get_lmd(n-1)
