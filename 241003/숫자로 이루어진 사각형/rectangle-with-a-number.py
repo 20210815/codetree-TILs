4fd4b5fec8f3b83ae1a35ceec311e49fab21aa20
이규민 <90364652+20210815@users.noreply.github.com>
2024-10-03 16:05:16 +0900

    Title: 숫자로 이루어진 사각형, Time: 125ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 241003/숫자로 이루어진 사각형/rectangle-with-a-number.py
@@ -0,0 +1,23 @@
+n = int(input())
+
+arr = [[_ for _ in range(n)] for _ in range(n)]
+
+def make_square(n):
+    for i in range(n):
+        for j in range(n):
+            if i * n + j + 1 <= 9:
+                arr[i][j] = i * n + j + 1
+            else:
+                arr[i][j] = i * n + j + 1 - 9
+    print_arr(n)
+
+def print_arr(n):
+    for i in range(n):
+        for j in range(n):
+            print(arr[i][j], end=" ")
+        print()
+#1234
+#5678
+
+
+make_square(n)
