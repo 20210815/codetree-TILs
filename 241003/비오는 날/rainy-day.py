7bcf40c4f6a393505d54b128289bc1ce25bcdf1f
이규민 <90364652+20210815@users.noreply.github.com>
2024-10-03 18:03:28 +0900

    Title: 비오는 날, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241003/비오는 날/rainy-day.py
@@ -0,0 +1,21 @@
+class Weather:
+    def __init__ (self, date, day, wea):
+        self.date = date
+        self.day = day
+        self.wea = wea
+
+
+n = int(input())
+
+arr = [tuple(input().split()) for _ in range(n)]
+guess = [Weather(date, day, wea) for date, day, wea in arr]
+
+target_id = 0
+min_date = "2100-00-00"
+for i, weat in enumerate(guess):
+    if weat.wea == 'Rain':
+        if min_date < weat.date:
+            min_date = weat.date
+            target_id = i
+
+print(f'{guess[target_id+1].date} {guess[target_id+1].day} {guess[target_id+1].wea}')
