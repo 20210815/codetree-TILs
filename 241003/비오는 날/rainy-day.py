6f91ac74233522a80a2e6839492d0f10ee595e4c
이규민 <90364652+20210815@users.noreply.github.com>
2024-10-03 18:04:29 +0900

    Title: 비오는 날, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

--- 241003/비오는 날/rainy-day.py
+++ 241003/비오는 날/rainy-day.py
@@ -14,8 +14,8 @@ target_id = 0
 min_date = "2100-00-00"
 for i, weat in enumerate(guess):
     if weat.wea == 'Rain':
-        if min_date < weat.date:
+        if min_date > weat.date:
             min_date = weat.date
             target_id = i
 
-print(f'{guess[target_id+1].date} {guess[target_id+1].day} {guess[target_id+1].wea}')
+print(f'{guess[target_id].date} {guess[target_id].day} {guess[target_id].wea}')
