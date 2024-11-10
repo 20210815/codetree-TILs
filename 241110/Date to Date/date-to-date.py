b17a9176d67e0657c75d76829358efa00ef55146
이규민 <90364652+20210815@users.noreply.github.com>
2024-11-10 18:14:12 +0900

    Title: Date to Date, Time: 41ms, Memory: 15MB, Status: Passed - CodeTree

--- 241110/Date to Date/date-to-date.py
+++ 241110/Date to Date/date-to-date.py
@@ -3,14 +3,16 @@ num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 m1, d1, m2, d2 = tuple(map(int, input().split()))
 
 
-result = 0
-for i in range(m1-1, m2):
-    if m1-1 == i:
-        result += num_of_days[i] - d1
-    elif m2-1 == i:
-        result += d2
-    else:
-        result += num_of_days[i]
+day1 = 0
+day2 = 0
+for i in range(m1):
+    day1 += num_of_days[i]
 
+day1 += d1
 
-print(result)
+for i in range(m2):
+    day2 += num_of_days[i]
+
+day2 += d2
+
+print(day2-day1+1)
