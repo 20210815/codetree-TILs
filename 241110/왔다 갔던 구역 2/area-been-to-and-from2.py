4fe6dcdfadc65bd9f56866d2c2a12b3eae0be881
이규민 <90364652+20210815@users.noreply.github.com>
2024-11-10 23:51:00 +0900

    Title: 왔다 갔던 구역 2, Time: 42ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 241110/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 241110/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -2,19 +2,21 @@ n = int(input())
 
 arr = [0] * 40
 now = 20
+arr[now]+=1
 
 for i in range(n):
     x, W = tuple(map(str, input().split()))
     if W == "R":
-        for j in range(int(x)+1):
+        for j in range(int(x)):
             now += 1
             arr[now] += 1 
-    else:
-        for k in range(int(x)+1):
+            #print(arr)
+    else: 
+        # 왼쪽으로 7
+        for k in range(int(x)):
             now -= 1
             arr[now] += 1
-
-print(arr)
+            #print(arr)
 
 result = 0
 for i in range(len(arr)):
