70502b5b25d014656d41a5cee560989904942179
이규민 <90364652+20210815@users.noreply.github.com>
2024-11-10 18:51:22 +0900

    Title: 그 요일은, Time: 43ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 241110/그 요일은/the-day-of-the-day.py
+++ 241110/그 요일은/the-day-of-the-day.py
@@ -1,7 +1,7 @@
 m1, d1, m2, d2 = tuple(map(int, input().split()))
 day = input()
 
-
+week = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
 num_of_days = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 def calculateDate(m, d):
@@ -14,5 +14,9 @@ def calculateDate(m, d):
 
 date1 = calculateDate(m1, d1)
 date2 = calculateDate(m2, d2)
+term = date2-date1
 
-print((date2-date1+1)//7)
+if term % 7 == week.index(day):
+    print(term // 7 + 1)
+else:
+    print(term // 7)
