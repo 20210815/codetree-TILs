06644c15c81a781dd017809d23c8614c0cbb3af0
이규민 <90364652+20210815@users.noreply.github.com>
2024-11-10 23:22:08 +0900

    Title: 블럭쌓는 명령2, Time: 45ms, Memory: 15MB, Status: Passed - CodeTree

--- 241110/블럭쌓는 명령2/block-stacking-commands2.py
+++ 241110/블럭쌓는 명령2/block-stacking-commands2.py
@@ -6,7 +6,7 @@ arr = [0] * (N + 1)
 
 for i in range(K):
     s, e = tuple(map(int, input().split()))
-    for j in range(s, e):
-        arr[s] += 1
+    for j in range(s, e+1):
+        arr[j] += 1
 
-print(arr.index(max(arr))+1)
+print((max(arr)))
