949d0144498cc32b575243a37689bf3c450d2f17
이규민 <90364652+20210815@users.noreply.github.com>
2024-11-14 21:05:42 +0900

    Title: 모이자, Time: 48ms, Memory: 16MB, Status: Passed - CodeTree

--- 241114/모이자/gather.py
+++ 241114/모이자/gather.py
@@ -2,18 +2,14 @@ n = int(input())
 
 people = list(map(int, input().split()))
 
-goto = 0
-min_sum = 999
+min_sum = 99 * 100 * 99
 for i in range(n):
     # 내가 가려는 집이 i 라면 
-    goto = i
     # 이동 거리를 구해서
-
-    
     result = 0
         # 현재 집에서부터 - i 까지의 이동 거리
     for j in range(n):
-       result += abs(j - i) * people[j]
+        result += abs(j - i) * people[j]
     # 최소를 구함
     min_sum = min(result, min_sum)
 
