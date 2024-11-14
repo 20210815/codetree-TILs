f034a0341163b360ab5483610c2da40b591894e1
이규민 <90364652+20210815@users.noreply.github.com>
2024-11-14 19:06:24 +0900

    Title: 빙빙 돌며 사각형 채우기, Time: 84ms, Memory: 19MB, Status: Wrong Answer - CodeTree

---
+++ 241114/빙빙 돌며 사각형 채우기/snail-alphabet-square.py
@@ -0,0 +1,27 @@
+n, m = tuple(map(int, input().split()))
+
+arr = [['' for _ in range(m)] for _ in range(n)]
+
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+
+def inRange(x1, y1):
+    if x1 >= 0 and y1 >= 0 and x1 < n and y1 < m and arr[x1][y1] == '':
+        return True
+    else:
+        return False
+
+dir = 0
+x, y = 0, 0
+for i in range(0, n*m):
+    arr[x][y] = chr(ord('A') + i)
+
+    nx, ny = x + dx[dir], y + dy[dir]
+    if not inRange(nx, ny):
+        dir = (dir + 1) % 4
+    x, y = x + dx[dir], y + dy[dir]
+
+for i in range(n):
+    for j in range(m):
+        print(arr[i][j], end=" ")
+    print()
