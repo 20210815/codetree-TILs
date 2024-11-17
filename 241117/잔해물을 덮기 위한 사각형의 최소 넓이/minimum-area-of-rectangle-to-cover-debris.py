03cd0ef8a858b902570aaafd0c15f97298776a87
이규민 <90364652+20210815@users.noreply.github.com>
2024-11-17 13:03:37 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 81ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 241117/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -0,0 +1,30 @@
+OFFSET = 10
+MAX = 20
+
+def checkingList(x1, x2, y1, y2, k):
+    global arr
+    for i in range(x1, x2):
+        for j in range(y1, y2):
+            arr[i+OFFSET][j+OFFSET] = k
+
+x1, y1, x2, y2 = tuple(map(int, input().split()))
+
+rx1, ry1, rx2, ry2 = tuple(map(int, input().split()))
+
+arr = [[0 for _ in range(MAX)] for _ in range(MAX)]
+
+checkingList(x1, x2, y1, y2, 1)
+checkingList(rx1, rx2, ry1, ry2, 2)
+
+min_x, min_y = MAX, MAX
+max_x, max_y = 0, 0
+for i in range(MAX):
+    if arr[i].count(1) >= 1:
+        for j in range(MAX):
+            if arr[i][j] == 1:
+                min_x = min(i, min_x)
+                min_y = min(j, min_y)
+                max_x = max(i,max_x)
+                max_y = max(j, max_y)
+
+print((max_x-min_x+1) * (max_y - min_y+1))
