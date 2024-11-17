10fb6646a130a57cadd88f754b4bf386dd3ff909
이규민 <90364652+20210815@users.noreply.github.com>
2024-11-17 12:19:28 +0900

    Title: k개 중에 1개를 n번 뽑기, Time: 224ms, Memory: 26MB, Status: Passed - CodeTree

--- 241117/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.py
+++ 241117/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.py
@@ -3,7 +3,7 @@ K, N = tuple(map(int, input().split()))
 def dfs(n):
     global arr
     if n == N+1:
-        print(arr)
+        print(*arr)
         return
     for i in range(1, K+1):
         arr.append(i)
