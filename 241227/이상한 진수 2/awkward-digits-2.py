fb088b2c807019da8bfc8c37fa3b6286972a3cd3
이규민 <90364652+20210815@users.noreply.github.com>
2024-12-27 20:10:45 +0900

    Title: 이상한 진수 2, Time: 47ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241227/이상한 진수 2/awkward-digits-2.py
+++ 241227/이상한 진수 2/awkward-digits-2.py
@@ -9,9 +9,12 @@ for index in range(len(a)):
         break
 
 result = 2
+sqr = 2
 for index in range(len(a)):
     if int(a[index]) == 1:
         for j in range(len(a)-index-1):
-            result*=2
+            sqr*=2
+        result += sqr
+        sqr = 0
 
 print(result)
