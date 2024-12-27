56156f7b0866996bff7f3044fd380edd56020d4d
이규민 <90364652+20210815@users.noreply.github.com>
2024-12-27 20:18:00 +0900

    Title: 이상한 진수 2, Time: 42ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241227/이상한 진수 2/awkward-digits-2.py
+++ 241227/이상한 진수 2/awkward-digits-2.py
@@ -1,16 +1,21 @@
 a = list(input())
 
 count = 0
-for index in range(len(a)):
-    if int(a[index]) == 0 :
-        a[index] = '1'
-        count += 1
-    if count == 1:
-        break
 
+if len(a) == 1:
+    if a[0] == 1:
+        a[0] = '0'
+else:
+    for index in range(len(a)):
+        if int(a[index]) == 0 :
+            a[index] = '1'
+            count += 1
+        if count == 1:
+            break
+print(a)
 result = 0
 for index in range(len(a)):
     if int(a[index]) == 1:
-        result += 2 ** (len(a)-index-1)
+        result += (2 ** (len(a)-index-1)) * int(a[index])
 
 print(result)
