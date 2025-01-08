6bae90a8589cf92ec4781b6ca595bf7bb182336f
이규민 <90364652+20210815@users.noreply.github.com>
2025-01-08 20:12:03 +0900

    Title: 아름다운 수열 2, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

--- 250108/아름다운 수열 2/beautiful-sequence-2.py
+++ 250108/아름다운 수열 2/beautiful-sequence-2.py
@@ -5,7 +5,15 @@ B = list(map(int, input().split()))
 count = 0
 # Write your code here!
 for i in range(N-M+1):
-    if set(B) == set(A[i:i+M]):
-        count+=1
+    arr = A[i:i+M]
+    arr.sort()
+    B.sort()
+    for j in range(M):
+        if arr[j] != B[j]:
+            break
+        else:
+            if j == M-1:
+                count+=1
+
 
 print(count)
