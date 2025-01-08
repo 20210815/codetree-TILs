447a5644140a475e015e7716264187f19a87e48d
이규민 <90364652+20210815@users.noreply.github.com>
2025-01-08 20:04:07 +0900

    Title: 아름다운 수열 2, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250108/아름다운 수열 2/beautiful-sequence-2.py
+++ 250108/아름다운 수열 2/beautiful-sequence-2.py
@@ -5,11 +5,7 @@ B = list(map(int, input().split()))
 count = 0
 # Write your code here!
 for i in range(N-M+1):
-    for j in range(M):
-        if B[j] not in A[i:i+M]:
-            break
-        else:
-            if j == M-1:
-                #print(A[i:i+M])
-                count += 1
+    if set(B) == set(A[i:i+M]):
+        count+=1
+
 print(count)
