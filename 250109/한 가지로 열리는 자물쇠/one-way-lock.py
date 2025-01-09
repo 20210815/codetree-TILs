2e89d5af931fd1b7768ebf431ae2491e68c271b5
이규민 <90364652+20210815@users.noreply.github.com>
2025-01-09 23:34:59 +0900

    Title: 한 가지로 열리는 자물쇠, Time: 52ms, Memory: 17MB, Status: Passed - CodeTree

--- 250109/한 가지로 열리는 자물쇠/one-way-lock.py
+++ 250109/한 가지로 열리는 자물쇠/one-way-lock.py
@@ -9,7 +9,5 @@ for i in range(1, N+1):
         for k in range(1, N+1):
             if abs(k-c) <= 2 or abs(j-b) <= 2 or abs(i-a) <= 2:
                 result += 1
-            else:
-                break
 
 print(result)
