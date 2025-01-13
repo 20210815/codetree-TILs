cbffd0c0b72145ff4404e71e2c98acce7e8f0ec1
이규민 <90364652+20210815@users.noreply.github.com>
2025-01-13 23:49:14 +0900

    Title: 개발자의 능력 2, Time: 41ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250113/개발자의 능력 2/ability-of-developer-2.py
@@ -0,0 +1,29 @@
+import sys
+
+ability = list(map(int, input().split()))
+INT_MAX = sys.maxsize
+n = 6
+# Write your code here!
+
+def diff(i, j , k, l):
+    sum1 = ability[i] + ability[j]
+    sum2 = ability[k] + ability[l]
+    sum3 = sum(ability) - sum1 - sum2
+
+    ret = abs(sum1 - sum2)
+    ret = max(ret, abs(sum2-sum3))
+    ret = max(ret, abs(sum3-sum1))
+    return ret
+
+min_diff = INT_MAX
+for i in range(n):
+    for j in range(i+1, n):
+
+        #두번째 팀원
+        for k in range(n):
+            for l in range(k+1, n):
+                if k == i or k == j or k == l:
+                    continue
+                min_diff = min(min_diff, diff(i,j ,k, l))
+
+print(min_diff)
