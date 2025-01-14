64ea669e2fafe5a880943a58e7cc731f6ce8554d
이규민 <90364652+20210815@users.noreply.github.com>
2025-01-14 23:17:27 +0900

    Title: 두 선분, Time: 73ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 250114/두 선분/two-lines.py
@@ -0,0 +1,19 @@
+x1, x2, x3, x4 = map(int, input().split())
+
+# Write your code here!
+line = [0] * 100
+
+
+for i in range(x1, x2+1):
+    line[i] += 1
+
+for i in range(x3, x4+1):
+    line[i] += 1
+
+for i in range(len(line)):
+    if line[i] >= 2:
+        print('intersecting')
+        break
+    else:
+        if i == len(line) -1:
+            print('nonintersecting')
