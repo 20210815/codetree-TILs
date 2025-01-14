798c988ccf5b553dd5b66622da047c9856af2ded
이규민 <90364652+20210815@users.noreply.github.com>
2025-01-14 23:20:47 +0900

    Title: 두 선분, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 250114/두 선분/two-lines.py
+++ 250114/두 선분/two-lines.py
@@ -1,19 +1,13 @@
 x1, x2, x3, x4 = map(int, input().split())
 
 # Write your code here!
-line = [0] * 100000
-
-
-for i in range(x1, x2+1):
-    line[i] += 1
-
-for i in range(x3, x4+1):
-    line[i] += 1
-
-for i in range(len(line)):
-    if line[i] >= 2:
-        print('intersecting')
-        break
+def intersecting(x1, x2, x3, x4):
+    if x2 < x3 or x4 < x1:
+        return False
     else:
-        if i == len(line) -1:
-            print('nonintersecting')
+        return True
+
+if intersecting(x1, x2, x3, x4):
+    print('intersecting')
+else:
+    print('nonintersecting')
